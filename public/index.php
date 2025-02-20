81c981749df28bcade074e3fc65eced1758d947d
NeoEtIchiro <neoetichiro@gmail.com>
2025-02-20 08:16:48 +0100

    M

--- public/index.php
+++ public/index.php
@@ -15,7 +15,7 @@ if (isset($_SESSION['user'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
-    <title>Titre de la page</title>
+    <title>Titre de la</title>
 </head>
 <body class="min-h-screen flex flex-col">
     
